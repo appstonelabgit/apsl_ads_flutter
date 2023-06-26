cd3f44d4ece10ef3269ca29c8b0efa9735bc98e4
apsl <dbvaghani@gmail.com>
2023-06-26 14:45:53 +0530

    improvement in example

--- lib/src/utils/i_ad_id_manager.dart
+++ lib/src/utils/i_ad_id_manager.dart
@@ -2,45 +2,17 @@ abstract class IAdIdManager {
   const IAdIdManager();
 
   List<AppAdIds> get appAdIds;
-
-  // /// Pass null if you wish not to implement unity ads
-  // ///
-  // /// AppAdIds? get unityAdIds => null;
-  // AppAdIds? get unityAdIds;
-
-  // /// Pass null if you wish not to implement admob ads
-  // ///
-  // /// AppAdIds? get admobAdIds => null;
-  // AppAdIds? get admobAdIds;
-
-  // /// Pass null if you wish not to implement appLovin ads
-  // ///
-  // /// AppAdIds? get appLovinAdIds => null;
-  // AppAdIds? get appLovinAdIds;
-
-  // /// Pass null if you wish not to implement facebook ads
-  // ///
-  // /// AppAdIds? get fbAdIds => null;
-  // AppAdIds? get fbAdIds;
 }
 
 class AppAdIds {
-  /// App Id should never be null, if there is no app id for a particular ad network, leave it empty
+  /// Always pass appId, if adnetwork don't have app id pass it as empty string
   final String appId;
 
   /// if id is null, it will not be implemented
   final String? appOpenId;
-
-  /// if id is null, it will not be implemented
   final String? interstitialId;
-
-  /// if id is null, it will not be implemented
   final String? rewardedId;
-
-  /// if id is null, it will not be implemented
   final String? bannerId;
-
-  /// if id is null, it will not be implemented
   final String? nativeId;
 
   /// Ad network type
--- lib/src/utils/test_ad_id_manager.dart
+++ lib/src/utils/test_ad_id_manager.dart
@@ -20,6 +20,18 @@ class TestAdIdManager extends IAdIdManager {
           rewardedId: 'ca-app-pub-3940256099942544/5224354917',
         ),
         AppAdIds(
+          adNetwork: 'admob',
+          appId: Platform.isAndroid
+              ? 'ca-app-pub-3940256099942544~3347511713'
+              : 'ca-app-pub-3940256099942544~1458002511',
+          appOpenId: Platform.isAndroid
+              ? 'ca-app-pub-3940256099942544/3419835294'
+              : 'ca-app-pub-3940256099942544/5662855259',
+          bannerId: 'ca-app-pub-3940256099942544/6300978111',
+          interstitialId: 'ca-app-pub-3940256099942544/1033173712',
+          rewardedId: 'ca-app-pub-3940256099942544/5224354917',
+        ),
+        AppAdIds(
           adNetwork: 'unity',
           appId: Platform.isAndroid ? '4374881' : '4374880',
           bannerId: Platform.isAndroid ? 'Banner_Android' : 'Banner_iOS',
